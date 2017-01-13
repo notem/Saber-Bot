687613cb12a96036f325678e6ff6b3137226576b
nate mathews <njm3308@rit.edu>
2017-01-13 14:25:13 -0500

    Modified statsCommand

--- src/main/java/ws/nmathe/saber/commands/admin/StatsCommand.java
+++ src/main/java/ws/nmathe/saber/commands/admin/StatsCommand.java
@@ -5,6 +5,9 @@ import ws.nmathe.saber.commands.Command;
 import ws.nmathe.saber.utils.MessageUtilities;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
 /**
  */
 public class StatsCommand implements Command
@@ -24,9 +27,16 @@ public class StatsCommand implements Command
     @Override
     public void action(String[] args, MessageReceivedEvent event)
     {
-        String msg = "**Current Stats**\n";
-        msg += "  Schedule Entries: " + Main.getScheduleManager().getAllEntries().size() + "\n";
-        msg += "  Guilds: " + Main.getBotJda().getGuilds().size() + "\n";
+        String msg = "```python\n";
+        msg += "Entries: " + Main.getScheduleManager().getAllEntries().size() + "\n";
+        msg += "Guilds: " + Main.getBotJda().getGuilds().size() + "\n";
+        Runtime rt = Runtime.getRuntime();
+        msg += "Memory-total: " +rt.totalMemory()/1024/1024 + " MB\n" +
+                "      -free : " + rt.freeMemory()/1024/1024 + " MB\n" +
+                "      -max  : " + rt.maxMemory()/1024/1024 + " MB\n";
+        RuntimeMXBean rb = ManagementFactory.getRuntimeMXBean();
+        msg += "  Uptime: " + rb.getUptime()/1000/60 + " minute(s)";
+        msg += "```";
 
         MessageUtilities.sendPrivateMsg( msg, event.getAuthor(), null );
     }
