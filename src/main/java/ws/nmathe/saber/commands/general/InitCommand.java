a37dee9fc93095c2cceaafe0d254d7af7f9052ab
notem <njm3308@rit.edu>
2018-03-30 19:02:30 -0400

    feat: added check to !init to stop silly users from initializing on an active channel

--- src/main/java/ws/nmathe/saber/commands/general/InitCommand.java
+++ src/main/java/ws/nmathe/saber/commands/general/InitCommand.java
@@ -59,39 +59,52 @@ public class InitCommand implements Command
     @Override
     public String verify(String prefix, String[] args, MessageReceivedEvent event)
     {
-        String head = prefix + this.name();
-
-        if(!event.getGuild().getMember(event.getJDA().getSelfUser())
-                .getPermissions().contains(Permission.MANAGE_CHANNEL))
-        {
-            return "I need the Manage Channels permission to create a new schedule!";
-        }
-
         if(Main.getScheduleManager().isLimitReached(event.getGuild().getId()))
         {
-            return "You have reached the limit for schedules! Please remove one schedule channel before trying again.";
+            return "You have reached the limit for schedules! Please delete one of your guild's schedules before trying again.";
         }
-
         if(args.length > 1)
         {
-            return "That's too many arguments! Use ``" + head + " [<name>]``";
+            return "That's too many arguments! Use ``" + prefix + this.name() + " [<name>|<channel>]``";
         }
-
         if(args.length == 1)
         {
-            if(args[0].length()>100 || args[0].length()<2)
+            if (args[0].matches("<#[\\d]+>")) // first arg is a discord channel link
             {
-                return "Schedule name must be between 2 and 100 characters long!";
-            }
+                String chanId = args[0].replaceFirst("<#","").replaceFirst(">","");
+                String commandChannelId = Main.getGuildSettingsManager()
+                        .getGuildSettings(event.getGuild().getId()).getCommandChannelId();
+                if(chanId.equals(commandChannelId) || chanId.equals(event.getChannel().getId()))
+                {
+                    return "Your guild's command channel cannot be converted to a schedule!";
+                }
 
-            String chanId = args[0].replaceFirst("<#","").replaceFirst(">","");
-            String commandChannelId = Main.getGuildSettingsManager().getGuildSettings(event.getGuild().getId()).getCommandChannelId();
-            if(chanId.equals(commandChannelId) || chanId.equals(event.getChannel().getId()))
+                TextChannel channel = event.getGuild().getTextChannelById(chanId);
+                if (channel == null)
+                {
+                    return "I could not find the channel, " + args[0] + "!";
+                }
+
+                if (channel.getHistory().retrievePast(6).complete().size() > 5) // refuse to convert channels with messages
+                {
+                    return "That channel is not empty! I will not convert an active channel to a schedule channel!";
+                }
+            }
+            else // otherwise arg must be channel name
             {
-                return "Your guild's command channel cannot be converted to a schedule!";
+                if(args[0].length()>100 || args[0].length()<2)
+                {
+                    return "Schedule name must be between 2 and 100 characters long!";
+                }
+
+                boolean canCreateChannels = event.getGuild().getMember(event.getJDA().getSelfUser())
+                        .getPermissions().contains(Permission.MANAGE_CHANNEL);
+                if(!canCreateChannels)
+                {
+                    return "I need the Manage Channels permission to create a new schedule!";
+                }
             }
         }
-
         return "";
     }
 
@@ -115,11 +128,10 @@ public class InitCommand implements Command
 
             if(!isAChannel) // use the argument as the new channel's name
             {
-                String chanTitle = args[0].replaceAll("[^A-Za-z0-9_ -]","").replace(" ","_");
+                String chanTitle = args[0].replaceAll("[^A-Za-z0-9_ \\-]","").replaceAll("[ ]","_");
                 Main.getScheduleManager().createSchedule(event.getGuild().getId(), chanTitle);
-                body = "A new schedule channel named **" + chanTitle.toLowerCase() + "** has been created!";
-
-                body += "\nYou can now use the create command to create events on that schedule, or the sync command to sync " +
+                body = "A new schedule channel named **" + chanTitle.toLowerCase() + "** has been created!\n" +
+                        "You can now use the create command to create events on that schedule, or the sync command to sync " +
                         "that schedule to a Google Calendar.";
             }
             else // convert the channel to a schedule
@@ -134,28 +146,24 @@ public class InitCommand implements Command
                                 finalChan.deleteMessageById(msgId).complete();
                                 Main.getEntryManager().removeEntry(document.getInteger("_id"));
                             });
-
                     body = "The schedule <#" + chanId + "> has been cleared!";
                 }
                 else
                 {   // create a new schedule
                     Main.getScheduleManager().createSchedule(chan);
-                    body = "The channel <#" + chanId + "> has been converted to a schedule channel!";
-
-                    body += "\nYou can now use the create command to create events on that schedule, or the sync command to sync " +
-                            "that schedule to a Google Calendar.";
+                    body = "The channel <#" + chanId + "> has been converted to a schedule channel!\n" +
+                            "You can now use the create command to create events on that schedule, or the sync " +
+                            "command to sync that schedule to a Google Calendar.";
                 }
             }
         }
         else // create a new schedule using the default name
         {
             Main.getScheduleManager().createSchedule(event.getGuild().getId(), null);
-            body = "A new schedule channel named **new_schedule** has been created!";
-
-            body += "\nYou can now use the create command to create events on that schedule, or the sync command to sync " +
-                    "that schedule to a Google Calendar.";
+            body = "A new schedule channel named **new_schedule** has been created!\n" +
+                    "You can now use the create command to create events on that schedule, or the sync command" +
+                    " to sync that schedule to a Google Calendar.";
         }
-
         MessageUtilities.sendMsg(body, event.getChannel(), null);
     }
 }
